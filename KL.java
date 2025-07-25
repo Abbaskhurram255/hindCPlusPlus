648a0a6f1f456392d09b4d0f274f5b2a010dac85
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2025-07-25 22:31:45 +0500

    Add files via upload

--- KL.java
+++ KL.java
@@ -17,7 +17,7 @@ import static java.lang.System.out;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.net.*;
-//gui
+//desktop gui with swing, and awt
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -26284,9 +26284,9 @@ public class KL {
 		try {
 			// refactoring alike specifiers by grouping them together
 			s = s.replaceAll("%l", "%d").replaceAll("[%\\{](\\.\\d)?db\\}?",
-					"%$1f");
-			s = s.replaceAll("[%\\{]([dinf])[\\:\\.],2\\}?", "%$1")
-					.replaceAll("[%\\{]([dinf])[\\:\\.],3\\}?", "%$1u")
+					"%$1f").replaceAll("[%\\{]([dinf]):(th|r|p?x|uc?|c)\\}?", "%$1$2");
+			s = s.replaceAll("[%\\{]([dinf])\\:,2\\}?", "%$1")
+					.replaceAll("[%\\{]([dinf])\\:,3\\}?", "%$1u")
 					.replaceAll(
 							"[%\\{]([dinf])c?([\\:\\.][A-Za-z\\$\\€\\£\\₹\\¥]{1,4})\\}?",
 							"%$1c$2");
@@ -26673,7 +26673,7 @@ public class KL {
 						if (in(m, "[:=]")) {
 							if (in(m,
 									"[:=]{1,2}(?=\\.\\d(f|db)|,\\d?|((pk|in)r|rs)|usd|p?x|th|r|[A-Za-z]{3,4})")
-									&& (field instanceof Float
+									&& (field instanceof Integer || field instanceof Long || field instanceof Float
 											|| field instanceof Double)) {
 								if (!in(m, "[:=]{2}|\\\\[:=]")) {
 									label = "";
@@ -26700,7 +26700,7 @@ public class KL {
 								// of the
 								// field AS-IS
 							} else {
-								label = m.replaceAll("[\\$\\{\\\\\\}]", "");
+								label = m.replaceAll("[\\$\\{\\\\\\}]|(?<=[:=])\\w+", "");
 							}
 							// allows following behavior:
 							// {amount:.1f} returns the double or floating-point
@@ -34014,12 +34014,31 @@ public class KL {
 
 	public static String name = "Ayesha";
 	public static int age = 23;
-	public static float score = 300500.255f;
+	public static double score = 300500D;
 
 	public static void main(String[] args) {
-		print("{sentCase:hello} %dpx", 835000);
-		print("{f:inr}", score);
+		print("$sentCase:hello {{age}+3-9} {d:inr} {d:r}", 835000, 13);
+		print("%d:th", 5603);
 		print(f(500000.215));
+		print("{age\\:rs}");
+		print("{age:th}");
+		print("{age:r}");
+		print("{age::px}");
+		print("{score:}");
+		print("{score:.3f}");
+		print("{score:.2f}");
+		print("{score:.1f}");
+		print("{score:.0f}");
+		print("{score:x}");
+		print("{score::x}");
+		print("{score:px}");
+		print("{score::px}");
+		print("{score:rs}");
+		print("{score::rs}");
+		print("{score::}");
+		print("{score}");
+		print("Intl: {score:,3}");
+		print("PK:{score:,2}");
 		// print("Hi, it's $name, $age. $toRoman(&2+3) is my height.
 		// $upper(love). %nc is how much I want to earn coding. &4.2+.3",
 		// 736660.2);
