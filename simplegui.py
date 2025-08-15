0f56a19b25318f5755913b173e8761967569886c
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2025-08-15 15:03:45 +0500

    Update simplegui.py

--- simplegui.py
+++ simplegui.py
@@ -1,20 +1,37 @@
 from FreeSimpleGUI import *
 
-theme("DarkTeal4")
+theme("Reds")
 
 lay = [
-    [T("Please fill the following fields:")],
-    [T("Name", size=(15,1)), I(key="Name")],
+    [Text("Please fill the following fields:")],
+    [T("Email", size=(15,2)), I(key="email")],
+    [T("Password", size=(15, 2)), I(password_char="*", key="pwd"), B("Show", size=(4, 1), key="reveal-password")],
     [Submit(), Exit()]
 ]
 
 
-window = Window("Simple Data Entry Form", lay)
+ui = Window("Simple Data Entry Form", lay)
+password_hidden = True
 
 while True:
-    event, values = window.read()
+    event, values = ui.read()
     if event == WIN_CLOSED or event == "Exit":
         break
     elif event == "Submit":
-        print(event, values)
+        email, password = values["email"], values["pwd"]
+        print("Event: " + event)
+        print("{")
+        print(f"    Email={email},")
+        print(f"    Password={password}")
+        print("}")
+        if email == "abbaskhurram255@gmail.com" and password == "00000000":
+            popup(f"Logged in as {email}")
+        else:
+            popup("Invalid Credentials!")
+    elif event == "reveal-password":
+        if password_hidden:
+            ui["pwd"].update(password_char="")
+        else:
+            ui["pwd"].update(password_char="*")
+        password_hidden = not password_hidden
 window.close()
