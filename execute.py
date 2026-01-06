ebd016c73bdb5b7be3d9cdf445b3e29f3f7a3d2f
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2026-01-06 23:33:16 +0500

    Add files via upload

--- execute.py
+++ execute.py
@@ -2,7 +2,7 @@ import re
 from numbers import Number
 from typing import *
 from types import *
-import importlib
+import importlib, cmd, ctypes
 import KL_Py
 # ^ the import above is a MANDATORY imoort, and so is the following:
 from KL_Py import *
@@ -23,8 +23,14 @@ def get_initial_of(x: Any) -> Any:
 			out = False
 		case "list":
 			out = []
-		case "dict":
-			out = {}
+		case "numlist":
+			out = numlist()
+		case "intlist":
+			out = intlist()
+		case "fltlist":
+			out = fltlist()
+		case "dict" | "obj":
+			out = KL_Py.obj()
 		case _:
 			out = None
 	return out
@@ -107,7 +113,7 @@ def execute(filename: str) -> None:
         "e_auto": "enum.auto",
         "kism": "type",
         "func": "Callable",
-        r"(?:(?:return|out)(s(?:[_ ]an?)?|[_ ]kare)|gives[ _]an?)": "->",
+        r"(?:(?:return|out)(s(?:[_ ]an?)?|[_ ]kare)|gives[ _]an?|deta[ _]ek)": "->",
         #theres a difference between these two ^ √
         "Shayad": "Union",
         "isfunc": "callable",
@@ -150,11 +156,20 @@ def execute(filename: str) -> None:
         # added partial support for ''
         for i, string in enumerate(strings):
         	strings[i] = strings[i][1:-1]
-        	strings[i] = replace(strings[i], r"\{([^\}]*)\}", r"[$1]")
+        	strings[i] = replace(strings[i], r"\$(?=\{)", "")
+        	#strings[i] = replace(strings[i], r"\{([^\}]*)\}", r"[$1]")
         	# WARNING: this changes core Python f-string functionality for {}
+        	# actually, for the sake of commas, and spaces (as the $-based syntax can mess them up), let's allow both
+        	# so the use has a choice to either:
+        	# print "Name is $name, Age is $age"
+        	# which is messed up, as the dollar recognizes the comma as part of the template --- resulting  in a tuple of `({{name}},)`
+        	# or they could do {{{varname}}} for the problematic template, or a template with spaces:
+        	# print "Name is {name}, Age is $age"
+        	# which WORKS
+        	# and results in "Name is {{name}}, Age is {{age}}"
         	strings[i] = "f\"" + replace(replace(strings[i], r"(?<!\\)\$([^ \n\t]+)", r"{$1}####"), r"(?<=\})#{4}", "") + "\""
         	# the #### part helps get rid of a bug
-        	# this replaces previously remove {} functionality with new $-based functionality
+        	# this replaces previously remove {formatted_var} functionality with new $-based functionality
         	# WARNING: r"{$1}####" should be as is
         	# the additional whitespace keeps the whole function together
         	# ^ needed as-is
@@ -177,9 +192,9 @@ def execute(filename: str) -> None:
         code = replace(code, r"(?<=,) (?:a(nd|ur)|ya|(?:ke[_ ]?)?sath(me)?)\b", "")
         # sequence matters!
         code = replace(code, r"(?<![\t    \t])\b(?:fc|act|def) (?:main|start)(?:\([^\)\n\t]*\))?(?=(?: *-> *[\w\?]+)?\:)", "def main()")
-        code = replace(code, r"(?<!\d)\.{3}\s?(?<dict>[_A-Za-z]\w*)", "**$dict")
+        code = replace(code, r"(?<![\d ])\.{3}\s?(?<dict>[_A-Za-z]\w*)", "**$dict")
         # sequence
-        code = replace(code, r"(?<![\.\d])\.{2}\s?(?<list>[_A-Za-z]\w*)", "*$list")
+        code = replace(code, r"(?<![\.\d ])\.{2}\s?(?<list>[_A-Za-z]\w*)", "*$list")
         # handling mathematical operations
         # SEQUENCE MATTERS
         code = replace(code, r"(?<A>\-?\d*\.?\d+) *\^ *(?<B>\-?\d*\.?\d+)", "$A**$B")
@@ -191,33 +206,35 @@ def execute(filename: str) -> None:
         code = replace(code, r"(?<A>([\w\-\.]+|[\(\[\{\"\'](?:[\"\'\w\-\.]+[, ]*[\)\]\}]*)+[\)\]\}\"\'])) me (?<B>([\w\-\.]+|[\(\[\{\"\'](?:[\"\'\w\-\.]+[, ]*[\)\]\}]*)+[\)\]\}\"\']))(?: (?:mojud|shamil(?: hen?)?|(?:ko )?(?:d[ei]kh[aeio]|pa{1,2}ya)(?: (?:ga?ya|he))?|hen?))?", "$B in $A")
         code = replace(code, r"(?<B>([\w\-\.]+|[\(\[\{\"\'](?:[\"\'\w\-\.]+[, ]*[\)\]\}]*)+[\)\]\}\"\'])) (?<A>([\w\-\.]+|[\(\[\{\"\'](?:[\"\'\w\-\.]+[, ]*[\)\]\}]*)+[\)\]\}\"\'])) me(?: (?:mojud|shamil(?: hen?)?|(?:ko )?(?:d[ei]kh[aeio]|pa{1,2}ya)(?: (?:ga?ya|he))?|hen?))?", "$B in $A")
         #Number system
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?so\b", "*1" + "0"*2)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?hazar\b", "*1" + "0"*3)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?la{1,2}(?:c|kh)\b", "*1" + "0"*5)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?million\b", "*1" + "0"*6)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?crore?\b", "*1" + "0"*7)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?hund(?:o|red) ?(?:[\*_]|times|guna|mul)? ?million\b", "*1" + "0"*8)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?(?:ara?b|billion)\b", "*1" + "0"*9)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?(?:kharab|hund(?:o|red) ?(?:[\*_]|times|guna|mul)? ?billion)\b", "*1" + "0"*11)
-        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?trillion\b", "*1" + "0"*12)
-        # comes before
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?so\b", f"*1{'0'*2}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?hazar\b", f"*1{'0'*3}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?la{1,2}(?:c|kh)\b", f"*1{'0'*5}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?million\b", f"*1{'0'*6}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?crore?\b", f"*1{'0'*7}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?hund(?:o|red) ?(?:[\*_]|times|guna|mul)? ?million\b", f"*1{'0'*8}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?(?:ara?b|billion)\b", f"*1{'0'*9}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?(?:kharab|hund(?:o|red) ?(?:[\*_]|times|guna|mul)? ?billion)\b", f"*1{'0'*11}")
+        code = replace(code, r"(?<=\d) ?(?:[\*_]|times|guna|mul)? ?trillion\b", f"*1{'0'*12}")
+        # come before
+        code = replace(code, r"(?<=[A-Za-z_])\s?(\.\.(?!\.))\s?(?=[^\:]+\:)", " in ")
         code = replace(code, r"(?<n1>\-?\d*\.?\d+)\s?(?:\.\.|se)\s?(?<n2>\-?\d*\.?\d+)", "range($n1, $n2)")
         # sequence
         # comes after
-        code = replace(code, r"(?<!\w)(?<nA>\-?\d*\.?\w+) in (?<nB>\-?\d*\.?\d+)\b", "$nA/$nB")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka a{1,2}thwa|eighth|into eight)\b", ".125*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka chotha|(?:in )(quarter|four)|quartered|fou?rth)\b", ".25*$n")
-        code = replace(code, r"\b(?:adh[aei]|hal(?:ved|f[ _](?:of|as))) (?<n>\-?\d*\.?\w+)\b", ".5*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka adha|halved|in (?:two|half))\b", ".5*$n")
-        code = replace(code, r"\bpon[ae] (?<n>\w+)\b", "-.25+$n")
-        code = replace(code, r"\bsawa (?<n>\w+)\b", ".25+$n")
-        code = replace(code, r"\bsa{1,2}dhe (?<n>\w+)\b", ".5+$n")
-        code = replace(code, r"\b(?:twice(?:[_ ]as)?|d[uo] ?gu?n[aei]) (?<n>\-?\d*\.?\w+)\b", "2*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+)( ka)? (?:twice|d[uo] ?gu?n[aei])\b", "2*$n")
-        code = replace(code, r"\b(?:thrice(?:[_ ]as)?|teen gun[aei]) (?<n>\-?\d*\.?\w+)\b", "3*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+)( ka)? (?:thrice|teen gun[aei])\b", "3*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:cha{1,2}r|4) gu?n[aei]\b", "4*$n")
-        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:a{1,2}th|8) gu?n[aei]\b", "8*$n")
+        # to avoid conflict
+        code = replace(code, r"(?<!\w)(?<nA>\-?\d*\.?\w+) in (?<nB>\-?\d*\.?\d+)\b", "($nA/$nB)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka a{1,2}thwa|eighth|into eight)\b", "(.125*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka chotha|(?:in )(quarter|four)|quartered|fou?rth)\b", "(.25*$n)")
+        code = replace(code, r"\b(?:adh[aei]|hal(?:ved|f[ _](?:of|as))) (?<n>\-?\d*\.?\w+)\b", "(.5*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:ka adha|halved|in (?:two|half))\b", "(.5*$n)")
+        code = replace(code, r"\bpon[ae] (?<n>\w+)\b", "(-.25+$n)")
+        code = replace(code, r"\bsawa (?<n>\w+)\b", "(.25+$n)")
+        code = replace(code, r"\bsa{1,2}dhe (?<n>\w+)\b", "(.5+$n)")
+        code = replace(code, r"\b(?:twice(?:[_ ]as)?|d[uo] ?gu?n[aei]) (?<n>\-?\d*\.?\w+)\b", "(2*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+)( ka)? (?:twice|d[uo] ?gu?n[aei])\b", "(2*$n)")
+        code = replace(code, r"\b(?:thrice(?:[_ ]as)?|teen gun[aei]) (?<n>\-?\d*\.?\w+)\b", "(3*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+)( ka)? (?:thrice|teen gun[aei])\b", "(3*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:cha{1,2}r|4) gu?n[aei]\b", "(4*$n)")
+        code = replace(code, r"(?<!\w)(?<n>\-?\d*\.?\w+) (?:a{1,2}th|8) gu?n[aei]\b", "(8*$n)")
         # ([..params(, *)?]*) -> lambda [..params(, *)?]:
         # e.g.
         # `(x) ->` becomes lambda x:
@@ -235,8 +252,8 @@ def execute(filename: str) -> None:
         # handle (?<=cls )`B (of|from|>|ext(ends)?|is_?an?) A` cases
         # sequence matters
         # _str, _eq -> __str__, __eq__
-        code = replace(code, r"(?<!\w)\._([A-Za-z]+)\b(?=\()", "self.__$1__")
-        code = replace(code, r"(?<!\w)\.([A-Za-z]\w*)\b(?=\()", "self.$1")
+        code = replace(code, r"(?<=\s)\._([A-Za-z]+)\b(?=\()", "self.__$1__")
+        code = replace(code, r"(?<=\s)\.([A-Za-z]\w*)\b(?=\()", "self.$1")
         # sequence
         code = replace(code, r"(?<=\w\.)_([A-Za-z]\w*)\b(?=\()", r"__$1__")
         # sequence
@@ -269,7 +286,7 @@ def execute(filename: str) -> None:
         code = replace(code, r"(?<k>[A-Za-z]\w*)(?: *: *(?<type>[\w\[\]\|, ]+\??))? *-> *(?<v>[^\n\t]+)", "\"$k\": $v,")
         # converting dicts to objs to allow the use of dot-driven access to keys
         # NOTE: does not support sub-dictionaries yet
-        code = replace(code, r"(?<!obj\()(\{ *[\"']?[\w.\-]+[\"']? *: *[^\{\}]+\})", "KL_Py.obj($1)")
+        code = replace(code, r"(?<!obj\()(\{[\s\n\t]*[\"']?[\w.\-]+[\"']? *: *[^\{\}]+\})", "KL_Py.obj($1)")
         # core
         code = replace(code, r"\bf(?=__STRING_\d+__)", "")
         # sequence matters
@@ -351,7 +368,8 @@ def execute(filename: str) -> None:
         		delattr(platform, k)
         		#setattr(platform, replace(k, "python", "klang"), "")
         (sys.version, sys.version_info, sys.executable, sys.pycache_prefix) = (builtins.version, builtins.version, "", "")
-        extended_builtins: dict[str, Any] = {"builtins": builtins, "print": cust_print, "range": rng, "Number": Number, "sys": sys, "platform": platform, "__name__": "__main__"}
+        extended_builtins: dict[str, Any] = {"builtins": builtins, "print": cust_print, "range": rng, "Number": Number, "sys": sys, "platform": platform, "KL_Py": KL_Py, "__name__": "__main__"}
+        # let's ALSO push every individual function from KL_Py
         add_module("KL_Py", extended_builtins)
         namespace: dict[str, Any] = extended_builtins | {}
         exec(code, namespace)
@@ -363,12 +381,12 @@ def execute(filename: str) -> None:
         	namespace[name] = obj
         if "main" in namespace and callable(namespace["main"]):
         	namespace["main"]()
-        keywords: list[str] = find_matches(code, r"(?<!\.)[_A-Za-z]\w*(?!\")")
-        for keyword in keywords:
-        	if keyword in namespace:
-        		if isinstance(namespace[keyword], str):
-        			print("do something for me")
-        		print(f"{keyword=}, work on it")
+        #keywords: list[str] = find_matches(code, r"(?<!\.)[_A-Za-z]\w*(?!\")")
+        #for keyword in keywords:
+        	#if keyword in namespace:
+        		#if isinstance(namespace[keyword], str):
+        			#print("do something for me")
+        		#print(f"{keyword=}, work on it")
         	
         	
 # let's try, and avoid some multi-main function conflict
@@ -391,17 +409,21 @@ class VariableNaMojudError(NameError):
 		self.name = name
 		self.message = message
 		super().__init__(name, message)
+class KeyNaMojudError(KeyError):
+	def __int__(self, name: str, message: str = " (key) is object/class me mojud nahi"):
+		self.name = name
+		self.message = message
+		super().__init__(name, message)
 class GalatValueError(ValueError):
 	def __int__(self, name: str, message: str = " ki value galat he"):
 		self.name = name
 		self.message = message
 		super().__init__(name, message)
-		
-		
-import cmd, ctypes
+
+TEMP_FILE_DIR: str = "._temp.klang"
 
 class Klang(cmd.Cmd):
-    prompt: str = "Klang>> "
+    prompt: str = "Klang> "
     intro: str = 'Klang version 0.8\n2025 build\nType "help" or "?" for commands.'
     version: str = "v0.8"
     credits: str = "__________________[Credits]______________________\nCore developer\t\t  ~ Khurram Ali"
@@ -431,22 +453,53 @@ class Klang(cmd.Cmd):
             	else:
             		# otherwise,
             		# interpret the line
-            		if not re.search(r"^(?:print|kaho)[ \(]+", line):
-            		    line = "print " + line
-            		line = "fc main():\n\t" + line
-            		with open("._temp.klang", "w") as temp_program_file:
-            		    if os.name == "nt":
-            		    	# hide the file on Windows
-            		    	# the . prefix already hides it on Unix-like platforms
-            		    	# including Linux, Android, and Mac
-            		    	HIDDEN: int = 2
-            		    	ctypes.windll.kernel32.SetFileAttributesW(temp_program_file.name, HIDDEN)
-            		    temp_program_file.write(line)
-            		    run_process(f"python execute.py {temp_program_file.name}", True)
-            	Klang().cmdloop()
+            		argument_variable: str = line
+            		contents: str = ""
+            		new_content: str = ""
+            		lines: list[str] = []
+            		last_line: str = ""
+            		if File(TEMP_FILE_DIR).exists_file():
+            			with open(TEMP_FILE_DIR, "r") as temp_program_file:
+            			    contents = temp_program_file.read().strip()
+            			    print(f"initially, {contents=}")
+            			    lines = contents.split("\n")
+            			    print(f"{lines=}")
+            			    if len(lines) > 0:
+            			    	last_line = replace(lines[-1], r"[\"'][^\"']*[\"']", "__STRING__")
+            			    	for line in lines:
+            			            print(f"{line=}")
+            			            if re.search(r"^[\s\t]*(print|kaho)", line.strip()):
+            			            	print("the line needs erasure")
+            			            	print("contents before:", contents)
+            			            	contents = replace(contents, line, "")
+            			            	print("contents AFTER line erasure:", contents)
+            			    print(f"new {contents=}")
+            		print(f"{last_line=}")
+            		if not re.search(r"^(print|kaho)[ \(]+", argument_variable) and "=" not in replace(argument_variable, r"[\"'][^\"']*[\"']", "__STRING__"):
+            		    argument_variable = "print " + argument_variable
+            		    print(f"{argument_variable=}")
+            		if not contents or "=" not in replace(contents, r"[\"'][^\"']*[\"']", "__STRING__"):
+            		    new_content = "fc main():\n\t" + argument_variable
+            		    print("contents empty")
+            		else:
+            		    new_content = contents + "\n\t" + argument_variable
+            		    print("contents non-empty")
+            		if os.name == "nt":
+            			# hide the file on Windows
+            		    # the . prefix already hides it on Unix-like platforms
+            		    # including Linux, Android, and Mac
+            		    HIDDEN: int = 2
+            		    ctypes.windll.kernel32.SetFileAttributesW(temp_program_file.name, HIDDEN)
+            		try:
+            		    with open(TEMP_FILE_DIR, "w") as temp_program_file:
+            		        temp_program_file.write(new_content)
+            		        run_process(f"python execute.py {temp_program_file.name}", True)
+            		except Exception as e:
+            		    print(e)
+            		Klang().cmdloop()
         else:
             print(f"Klang version 0.8\n\n{self.credits}")
-    do_do = do_klang
+    do_do = do_kl = do_klang
     def do_version(self, line) -> None:
         """Display the current version of Klang CLI"""
         print(self.version)
@@ -455,6 +508,14 @@ class Klang(cmd.Cmd):
         """Displays the author line"""
         print(self.credits)
     do_author = do_credits
+    def do_clear(self, line) -> bool:
+        """Clears the cache."""
+        if line != "cache":
+        	return
+        if not File(TEMP_FILE_DIR).exists_file():
+        	return
+        os.remove(TEMP_FILE_DIR)
+    do_cc = do_clear
     def do_quit(self, line) -> bool:
         """Closes the Klang terminal."""
         return True
@@ -464,7 +525,7 @@ class Klang(cmd.Cmd):
 		
 def main() -> None:
     arg: str
-    arg = argv[0] if len(argv) != 0 else "test.klang"
+    arg = argv[0] if len(argv) != 0 else "ttest.klang"
     if not arg.endswith(".klang"):
     	arg += ".klang"
     if not File(arg).is_file():
@@ -478,21 +539,30 @@ def main() -> None:
     	print("No entry point was found.\nEnter klang 'path/to/file' to compile.")
     	Klang().cmdloop()
     except SyntaxError as e:
-    	msg = replace(e.args[0], r"expected [\"\'](?<fix>\S+)[\"\']", "\"$fix\" ki umeed thi")
+    	args = list(e.args)
+    	args[0] = replace(args[0], r"\bunterminated\b", "ger khatm shuda")
+    	args[0] = replace(args[0], r"\b(?<=string)\sliteral\b", "")
+    	args[0] = replace(args[0], r"\bdetected\b", "pai gai")
+    	args[0] = replace(args[0], r"\bat\b", "karib")
+    	msg = replace(args[0], r"expected [\"\'](?<fix>\S+)[\"\']", "\"$fix\" ki umeed thi")
     	msg = f"\n    {msg} line {e.lineno} pe"
-    	if len(e.args) >= 2 and isinstance(e.args[1], tuple) and len(e.args[1]) >= 4:
-    		msg += f"\n    karib yaha: \"\n\t {e.args[1][3].strip()}\n    \"            ({e.args[0]}) ^^^\n\t\t\t        |||"
+    	if len(args) >= 2 and isinstance(args[1], tuple) and len(args[1]) >= 4:
+    		msg += f"\n    karib yaha: \"\n\t {args[1][3].strip()}\n    \"            ^^^\n\t\t\t    |||"
     		msg = replace(msg, r"f(?=\-string)", "k")
     	raise BuraSyntaxError(msg) from None
     except NameError as e:
-    	msg = find_match(e.args[0], r"[\"\'](\w+)[\"\']")
-    	msg = f"\n\tVariable \"{msg}\" is scope me mojud nahi"
+    	varname: str = find_match(e.args[0], r"[\"\'](\w+)[\"\']")
+    	msg = f"\n\tVariable \"{varname}\" is scope me mojud nahi"
     	raise VariableNaMojudError(msg) from None
+    except (KeyError, AttributeError) as e:
+       object: str = find_match(e.args[0], r"[\"\'](\w+)[\"\']")
+       key: str = find_match(e.args[0], r"[\"\'](\w+)[\"\']$")
+       msg = f"Class, ya object '{object}' me key '{key}' mojud nahi"
+       raise KeyNaMojudError(msg) from None
     except ValueError as e:
-    	msg = find_match(e.args[0], r"[\"\'](\w+)[\"\']")
-    	msg = f"\n\tValue \"{msg}\" ki umeed NAHI thi yaha,\n\t"
+    	value: str = find_match(e.args[0], r"[\"\'](\w+)[\"\']")
+    	msg = f"\n\tValue \"{value}\" ki umeed NAHI thi yaha,\n\t"
     	msg += replace(e.args[0], r"\b[Ii]nvalid\b", "galat")
-    	
     	raise GalatValueError(msg) from None
 
 if __name__ == "__main__":
